a29d68f9350394b572a22ac02efab06f74e664ba
Donatas <donkuprys@gmail.com>
2021-05-08 10:49:05 +0300

    Chek

--- js/chek.js
+++ js/chek.js
@@ -1,10 +1,21 @@
 function check(a, x) {
-    // your code here
+    console.log(a);
+
+    for(let i =0; i< a.length; i++){
+        const skaicius = (a[i]);
+
+        console.log (skaicius)
+        if (skaicius === x){
+            return true;
+        }
+        
+    }
+    return false
   }
 
   
-    console.log(([66, 101], 66), true);
-    console.log(([80, 117, 115, 104, 45, 85, 112, 115], 45), true);
-    console.log((['t', 'e', 's', 't'], 'e'), true);
-    console.log((['what', 'a', 'great', 'kata'], 'kat'), false);
+    console.log(check([66, 101], 66), true);
+    console.log(check([80, 117, 115, 104, 45, 85, 112, 115], 45), true);
+    console.log(check(['t', 'e', 's', 't'], 'e'), true);
+    console.log(check(['what', 'a', 'great', 'kata'], 'kat'), false);
     
