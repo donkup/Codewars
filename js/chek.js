c7e496dc7c2cbda41e8c04ddcace7b776cb54c69
Donatas <donkuprys@gmail.com>
2021-05-08 10:33:27 +0300

    getPlanet

---
+++ js/chek.js
@@ -0,0 +1,10 @@
+function check(a, x) {
+    // your code here
+  }
+
+  
+    console.log(([66, 101], 66), true);
+    console.log(([80, 117, 115, 104, 45, 85, 112, 115], 45), true);
+    console.log((['t', 'e', 's', 't'], 'e'), true);
+    console.log((['what', 'a', 'great', 'kata'], 'kat'), false);
+    
