5350803ff0838d44eb6ce14255a9596c261858d4
Donatas <donkuprys@gmail.com>
2021-05-10 20:18:00 +0300

    briks su Math

--- js/bricks.js
+++ js/bricks.js
@@ -6,20 +6,36 @@ UZDUOTIS:
 - gaunam sienos auksti, kuris visur yra vienodas
 - turime plytos dydi(aukstis ir plotis) (metrais)
 - reikia suzinoti, keliu plytu mums reikes, jei negalima skaldyti plytu
-
+- // Math.ceil (apvalina i didesne puse (1.2= 2))
+- // Math.floor (apvalina i mazesne puse (1.9 = 1))
+- // Math.round (apvalina nuo 0.5 i virsu)
 */
 
 function bricks(sienuPlociai, aukstis, plyosAukstis, plytosPlotis) {
-    let bendraSienu = 0;
+        let bendrasSienuPlotis = 0;
 
-    for (let i = 0; i < sienuPlociai.length; i++) {
-         bendraSienu += sienuPlociai[i];
+        for (let i = 0; i < sienuPlociai.length; i++) {
+            const sienosPlotis = sienuPlociai[i];
+            bendrasSienuPlotis += sienosPlotis;
+            
+        }
+        
+        const plytosAukstyn = Math.ceil(aukstis / plyosAukstis);
+        const plytosPlotyje = Math.ceil (bendrasSienuPlotis / plytosPlotis);
+        return plytosAukstyn * plytosPlotyje;
+
+
+
+    // let bendraSienu = 0;
+
+    // for (let i = 0; i < sienuPlociai.length; i++) {
+    //      bendraSienu += sienuPlociai[i];
                
-    }
+    // }
     
-    const kiekPlytuPlotyje = bendraSienu / plytosPlotis;
-    const kiekPlytuAukstyje = aukstis / plyosAukstis;
-    return kiekPlytuAukstyje * kiekPlytuPlotyje;
+    // const kiekPlytuPlotyje = bendraSienu / plytosPlotis;
+    // const kiekPlytuAukstyje = aukstis / plyosAukstis;
+    // return kiekPlytuAukstyje * kiekPlytuPlotyje;
     
 }
 
